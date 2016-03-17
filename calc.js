d8d635b2ca9d9479743916053f83e15025ebb48f
machung <kuanghung@gmail.com>
2016-03-17 23:38:01 +0800

    add substract function in calc.js

---
+++ calc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = class Calc{
+    constructor(){
+        
+    }
+    
+    add(a, b){
+        return a + b;
+    }
+    
+    substract(a, b){
+        return a - b;
+    }
+}
